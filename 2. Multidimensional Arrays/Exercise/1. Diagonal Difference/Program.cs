0c5c052c6d55030b74127cc1002336669743b4c2
Momchil Agapov <m.agapov2016@gmail.com>
2024-01-16 12:04:20 +0200

    C#

---
+++ 2. Multidimensional Arrays/Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,27 @@
+﻿int size = int.Parse(Console.ReadLine());
+int sumD1 = 0;
+int sumD2  = 0;
+
+int[,] matrix = new int[size, size];
+
+for (int row = 0; row < size; row++)
+{
+    int[] newRow = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+	for (int col = 0; col < size; col++)
+	{
+		matrix[row, col] = newRow[col];
+
+		if (row == col)
+		{
+			sumD1 += matrix[row, col];
+		}
+    }
+}
+
+for (int row = 0; row < size; row++)
+{
+    sumD2 += matrix[row, size - 1 - row];
+}
+
+Console.WriteLine(Math.Abs(sumD1 - sumD2));
---
+++ 2. Multidimensional Arrays/Exercise/2. Squares in Matrix/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] sizes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int rows = sizes[0];
+int cols = sizes[1];
+int counter = 0;
+
+string[] previousRow = null;
+
+for (int row = 0; row < rows; row++)
+{
+    string[] currentRow = Console.ReadLine().Split(' ');
+
+	if (row > 0)
+	{
+		for (int col = 0; col < cols - 1; col++)
+		{
+			if (currentRow[col] == previousRow[col] 
+				&& previousRow[col + 1] == currentRow[col] 
+				&& currentRow[col + 1] == previousRow[col])
+			{
+				counter++;
+			}
+		}
+
+	}
+
+    previousRow = currentRow;
+}
+
+Console.WriteLine(counter);
