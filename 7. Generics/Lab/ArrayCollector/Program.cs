bccbd21114931fd52896e825141b3d33eb696aa8
Momchil Agapov <m.agapov2016@gmail.com>
2024-02-05 18:58:50 +0200

    C#

---
+++ 7. Generics/Lab/ArrayCollector/Program.cs
@@ -0,0 +1,14 @@
+﻿using ArrayCollector;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+
+            int[] integers = ArrayCreator.Create(10, 33);
+        }
+    }
+}
