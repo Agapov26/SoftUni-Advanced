7a90b2133ea64e050c47004c7f461d3706534a4e
Momchil Agapov <m.agapov2016@gmail.com>
2024-02-25 19:23:46 +0200

    C#

---
+++ C# OOP/2. Encapsulation/Lab/First and Reserve Team/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                string firstName = cmdArgs[0];
+                string lastName = cmdArgs[1];
+                int age = int.Parse(cmdArgs[2]);
+                decimal salary = decimal.Parse(cmdArgs[3]);
+
+                var person = new Person(firstName, lastName, age, salary);
+
+                persons.Add(person);
+            }
+
+            var parcentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
