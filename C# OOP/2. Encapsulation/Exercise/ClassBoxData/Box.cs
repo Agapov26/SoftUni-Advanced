7ce8bc8911e14b91209a744417387367c4c02796
Momchil Agapov <m.agapov2016@gmail.com>
2024-02-28 17:07:02 +0200

    C#

---
+++ C# OOP/2. Encapsulation/Exercise/ClassBoxData/Box.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+        private const string ZeroOrNegativeArgumentException = "{0} cannot be zero or negative.";
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(String.Format(ZeroOrNegativeArgumentException, nameof(Length)));
+                }
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(String.Format(ZeroOrNegativeArgumentException, nameof(Width)));
+                }
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(String.Format(ZeroOrNegativeArgumentException, nameof(Height)));
+                }
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * ((Width * Height) + (Length * Height) + (Length * Width));
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * ((Length * Height) + (Width * Height));
+        }
+
+        public double Volume()
+        {
+            return Height * Width * Length;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Surface Area - {SurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {LateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {Volume():f2}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/2. Encapsulation/Exercise/PizzaCalories/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PizzaCalories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string pizzaName = Console.ReadLine().Split()[1];
+                string[] doughInput = Console.ReadLine().Split();
+                Dough dough = new Dough(doughInput[1], doughInput[2], int.Parse(doughInput[3]));
+
+                Pizza pizza = new Pizza(pizzaName, dough);
+
+                string[] toppingInput;
+                while ((toppingInput = Console.ReadLine().Split())[0] != "END")
+                {
+                    pizza.AddTopping(new Topping(toppingInput[1], int.Parse(toppingInput[2])));
+                }
+
+                Console.WriteLine($"{pizza.Name} - {pizza.TotalCalories:f2} Calories.");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
