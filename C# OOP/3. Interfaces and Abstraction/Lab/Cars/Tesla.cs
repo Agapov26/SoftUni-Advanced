6c6dc3ac3d636ebbc41f6b96924f0a7ebc7e38da
Momchil Agapov <m.agapov2016@gmail.com>
2024-03-01 17:44:51 +0200

    C#

---
+++ C# OOP/3. Interfaces and Abstraction/Lab/Cars/Tesla.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int batteries)
+        {
+            Model = model;
+            Color = color;
+            Batteries = batteries;
+        }
+
+        public string Model { get; private set; }
+        public string Color { get; private set; }
+        public int Batteries { get; private set; }
+
+        public string Start()
+        {
+            return "Start";
+        }
+        public string Stop()
+        {
+            return "Stop";
+        }
+
+        public override string ToString()
+        {
+            return $"{Model}, {Color}, {Batteries}";
+        }
+    }
+}
