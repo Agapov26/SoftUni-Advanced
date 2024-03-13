cbb9c5dfc0c73100f6318fe49f8c70384fcd3c3e
Momchil Agapov <m.agapov2016@gmail.com>
2024-03-13 18:05:21 +0200

    C#

---
+++ C# OOP/5. Reflection and Attributes/Lab/Collector/Spy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string CollectGettersAndSetters(string investigatedClass)
+        {
+            Type typeOfClass = Type.GetType(investigatedClass);
+            MethodInfo[] methods = typeOfClass.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (MethodInfo method in methods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (MethodInfo method in methods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
