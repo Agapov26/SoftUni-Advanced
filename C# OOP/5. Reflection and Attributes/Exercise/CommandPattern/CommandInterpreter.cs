aa607009cc72c1f3ab13caf35847b54757a78686
Momchil Agapov <m.agapov2016@gmail.com>
2024-03-14 19:41:48 +0200

    C#

---
+++ C# OOP/5. Reflection and Attributes/Exercise/CommandPattern/CommandInterpreter.cs
@@ -0,0 +1,32 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] parts = args.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string command = $"{parts[0]}Command";
+
+            var commandArgs = parts.Skip(1).ToArray();
+            var assmebly = Assembly.GetEntryAssembly();
+
+            Type type = assmebly.GetTypes().FirstOrDefault(t => t.Name == command);
+
+            if (type == null)
+            {
+                throw new ArgumentException("Invalid type!");
+            }
+
+            var instance =  (ICommand)Activator.CreateInstance(type);
+            return instance?.Execute(commandArgs);
+        }
+    }
+}
---
+++ C# OOP/5. Reflection and Attributes/Exercise/CommandPattern/Engine.cs
@@ -0,0 +1,37 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true) 
+            {
+                string input = Console.ReadLine();
+                string result = string.Empty;
+                try
+                {
+                    result = commandInterpreter.Read(input);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
